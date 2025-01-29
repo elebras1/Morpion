b94aab1121f562fb4709451e332bc8e70ea1b985
elebras1 <elebras1@gmail.com>
2025-01-29 11:37:01 +0100

    Ajout d'un serveur
    Ajout du server qui permettra d'accepter les clients et echanger les
    positions

---
+++ src/main/java/org/sysinfo/morpion/network/ServerMorpion.java
@@ -0,0 +1,40 @@
+package org.sysinfo.morpion.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerMorpion {
+    private static final int port = 2048;
+    private static List<BufferedReader> clientsIn = new BufferedReader<>();
+    private static List<PrintWriter> clientsOut = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Serveur start");
+
+            for (int i = 0; i < 2; i++) {
+                Socket clientSocket = serverSocket.accept();  // Accept client connection
+                System.out.println("Player " + (i + 1) + " connected.");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                clientsIn.add(in);
+                clientsOut.add(out);
+
+                out.println("Player " + (i + 1));
+                out.println("Waiting other player...");
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
