c199a878d09e70f220571a127dfe702a0d5e568b
MonUserGit <sully.millet.29@gmail.com>
2025-01-29 11:01:40 +0100

    Création de la branch sully_dev et ajout package game et class Player
    Ajout de la classe Player avec constructeur et setter, getter ainsi que
    ces attribut

--- src/main/java/org/sysinfo/morpion/game/Player.java
+++ src/main/java/org/sysinfo/morpion/game/Player.java
@@ -2,16 +2,37 @@ package org.sysinfo.morpion.game;
 
 public class Player {
     private String name;
-    private String pion;
+    private String pawn;
     private int nbWin;
     private boolean canPlay;
 
-    public Player (String name, String pion){
+    public Player (String name, String pawn){
         this.name = name;
-        this.pion = pion;
+        this.pawn = pawn;
         this.nbWin = 0;
     }
 
+    public String getPawn() {
+        return pawn;
+    }
 
+    public void setPion(String pion) {
+        this.pawn = pion;
+    }
 
+    public int getNbWin() {
+        return nbWin;
+    }
+
+    public void setNbWin(int nbWin) {
+        this.nbWin = nbWin;
+    }
+
+    public boolean isCanPlay() {
+        return canPlay;
+    }
+
+    public void setCanPlay(boolean canPlay) {
+        this.canPlay = canPlay;
+    }
 }
